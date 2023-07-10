068330dc7faf896091deecb28a68c0c6a6407cd5
Maryan Smoliy <smoliymaryan89@gmail.com>
2023-07-10 10:45:37 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import { Profile } from './Profiles/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" data={data} />
+      <FriendList friends={friends} />
+    </>
   );
 };
