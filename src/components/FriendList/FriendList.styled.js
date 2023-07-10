153d62e5e5eaba262077b956b45f9ebe94d318a8
Maryan Smoliy <smoliymaryan89@gmail.com>
2023-07-10 10:45:57 +0300

    Create FriendList.styled.js

---
+++ src/components/FriendList/FriendList.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const FriendListWrap = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  margin-bottom: 50px;
+`;
